01c09a06708f51ff1f46f0a65e07c4d82d5e412a
Diba Shaikh <dibanisa44@gmail.com>
2024-03-16 23:52:44 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,97 @@
+// //2= Task
+ let Fname:string=("Diba");
+console.log(`Hello  ${Fname} , would you like to learn some typescript?`);
+
+// // 3= Task
+
+
+let yourname : string = ("Diba Shaikh");
+console.log(yourname.toUpperCase())
+console.log(yourname.toLowerCase())
+console.log(yourname.charAt(0). toUpperCase() + yourname. slice(1). toLowerCase())
+
+// //4= Task
+
+
+console.log(`Napolean Hill syas "If you cannaot do great things,
+do small things in a great way."`);
+
+// // 5=Task
+
+
+let FamouseQuote:string=("Napolean Hil");
+console.log(`${FamouseQuote}, Says "If you cannaot do great things,
+do small things in a great way." `)
+
+// 6= Task
+// Print name with extra space:
+
+let D : string = "\t\n Diba Shaikh \t\n";
+console.log(D);
+console.log(D.trim());
+
+// // 7= Task
+// Print number 8 with using Arithmetic operators.
+
+console.log(2*4);
+console.log(10-2);
+console.log(6+2);
+console.log(16/2);
+
+//8= Task
+// Basic operation to display the number 8.
+
+console.log(4*2);
+console.log(15-7);
+console.log(6+2);
+console.log(64/8);
+
+// 9= Task
+
+// Print my favrouite number
+
+let favrouitnumber = 7;
+console.log(`My favrouite number is: ${favrouitnumber}`)
+
+
+// 10= Task
+// Post comment in your code that computer ignores.
+
+// 09/03/2024 : Print the question for asking.
+
+let Firstname:string=("Diba");
+console.log(`Hello  ${Firstname} , would you like to learn some typescript?`);
+
+
+ // 11= Task 
+
+// Asking for learning something,
+
+let myname: string =("Diba shaikh");
+
+console.log(`Hello ${myname} "Would you like to learn some typescript today?" `)
+
+// 12 = Task
+
+// Used array to store my multiple friends name.
+
+let names : string[] =["Aqsa", "Shaikhzadi", "Nida", "Maira"];
+for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+
+let Ynames: string[]= ["Shaikh", "Ziddi", "Queeen", "Khan"]
+for(let i=0; i < names.length; i++){
+    console.log(Ynames[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
